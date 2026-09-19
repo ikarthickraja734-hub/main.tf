0002040fab2bbc1bc96b0a174bee1b352f321885
ikarthickraja734-hub <ikarthickraja734@gmail.com>
2026-09-19 14:26:27 +0530

    Update hall.tf

--- hall.tf
+++ hall.tf
@@ -15,7 +15,7 @@ provider "aws" {
 # --------------------
 # VPC
 # --------------------
-resource "vpc" "main" {
+resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
